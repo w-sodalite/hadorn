1f63d37448c69720b02eac420674586b0d46be37
w-sodalite <w-sodalite@hotmail.com>
2025-01-16 17:25:09 +0800

    调整文档位置

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,4 @@
 #[doc = include_str!("../README.md")]
-pub use hadorn_macro::*;
-
-#[doc(hidden)]
-pub mod __http {
-    pub use http::*;
-}
-
-#[doc(hidden)]
-pub mod __reqwest {
-    pub use reqwest::*;
-}
-
 pub trait Hadorn {
     ///
     /// reqwest client
@@ -26,4 +14,17 @@ pub trait Hadorn {
     /// the request default headers
     ///
     fn default_headers(&self) -> Option<&http::HeaderMap>;
+}
+
+// export hadorn macro
+pub use hadorn_macro::*;
+
+#[doc(hidden)]
+pub mod __http {
+    pub use http::*;
+}
+
+#[doc(hidden)]
+pub mod __reqwest {
+    pub use reqwest::*;
 }
